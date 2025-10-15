b7173ac407da59301aa836afd3a726ccd4810edc
vcs <99899703+vcsHB@users.noreply.github.com>
2025-10-15 23:49:14 +0900

    fix: Comments

--- Assets/01.Scripts/SoundSystem/SoundController.cs
+++ Assets/01.Scripts/SoundSystem/SoundController.cs
@@ -21,7 +21,12 @@ namespace MINISoundManage
             _pool.Clear();
         }
 
-
+        /// <summary>
+        /// Play Souond. Prevents the same clip from playing within a certain amount of time.
+        /// </summary>
+        /// <param name="soundSO">SoundData for Playing</param>
+        /// <param name="position">Play Position</param>
+        /// <returns>Generated SoundPlayer Instance</returns>
         public SoundPlayer PlaySound(SoundSO soundSO, Vector2 position)
         {
             if (soundSO == null || soundSO.clip == null)
@@ -30,27 +35,26 @@ namespace MINISoundManage
                 return null;
             }
 
-            // 2. 오디오 클립의 해시 코드 가져오기
+            // 2. Get HashCode to audioClip
             int clipHashCode = soundSO.clip.GetHashCode();
             float currentTime = Time.time;
 
-            // 3. 해시코드를 체크하여 일정 시간 내 재생 여부 확인
+            // 3. Check Hashcode
             if (_lastPlayTimes.TryGetValue(clipHashCode, out float lastPlayTime))
             {
-                // 마지막 재생 시간으로부터 경과된 시간 체크
+                // Check elapsed time since Last Playback Time
                 if (currentTime - lastPlayTime < _minSoundPlayInterval)
                 {
-                    // 일정 시간 내에 재생되었으므로 재생 취소
-                    // Debug.Log($"재생 취소: 클립 해시코드 {clipHashCode}가 {_minSoundPlayInterval}초 이내에 다시 요청됨.");
+                    // Debug.Log($"[MINISOUND] PlayCanceled: AudioClip HashCode {clipHashCode} was requested again within {_minSoundPlayInterval} seconds");
                     return null;
                 }
             }
 
-            // 4. 재생이 허용되면, 사운드 풀에서 SoundPlayer를 가져와 재생
+            // 4. Play Allowed
             SoundPlayer player = _pool.GetSoundPlayer(position);
             player.PlaySound(soundSO);
 
-            // 5. 마지막 재생 시간을 갱신
+            // 5. Refresh LastPlayTime
             _lastPlayTimes[clipHashCode] = currentTime;
 
             return player;
