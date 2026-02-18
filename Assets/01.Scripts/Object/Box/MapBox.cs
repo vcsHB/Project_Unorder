6142d3fea3afdcd2cf179d74bdf963ad3f0a0de1
vcs <hoonbot06@gmail.com>
2026-02-18 20:34:26 +0900

    feat: BoxBoader Shader

--- Assets/01.Scripts/Object/Box/MapBox.cs
+++ Assets/01.Scripts/Object/Box/MapBox.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,30 +6,37 @@ namespace Project_Unorder.ObjectManage
     public class MapBox : MonoBehaviour
     {
         public UnityEvent OnBoxSizingCompleteEvent;
+
         [Header("Essential Settings")]
-        [SerializeField] Transform _bottomBar;
-        [SerializeField] Transform _topBar;
-        [SerializeField] Transform _leftBar;
-        [SerializeField] Transform _rightBar;
+        [SerializeField] private Transform _bottomBar;
+        [SerializeField] private Transform _topBar;
+        [SerializeField] private Transform _leftBar;
+        [SerializeField] private Transform _rightBar;
         [SerializeField] private SpriteRenderer _edgeBoxRenderer;
 
         [Header("Box Setting")]
-
         [SerializeField] private Vector2 _boxSize = new Vector2(20f, 20f);
-        private Vector2 _previousSize;
         [SerializeField] private float _boxThickness = 0.25f;
+
+        private Vector2 _previousSize;
         private BoxWall[] _walls;
+        private SpriteRenderer[] _barRenderers;
         private float _currentDuration;
         private float _sizingDuration;
         private bool _isSizing;
 
+        private static readonly int _useLimitProperty = Shader.PropertyToID("_UseLimit");
 
         private void Awake()
         {
             _walls = GetComponentsInChildren<BoxWall>();
-        }
 
-        #region Debug
+            _barRenderers = new SpriteRenderer[4];
+            _barRenderers[0] = _bottomBar.GetComponent<SpriteRenderer>();
+            _barRenderers[1] = _topBar.GetComponent<SpriteRenderer>();
+            _barRenderers[2] = _leftBar.GetComponent<SpriteRenderer>();
+            _barRenderers[3] = _rightBar.GetComponent<SpriteRenderer>();
+        }
 
         [ContextMenu("Debug10")]
         private void DebugASd()
@@ -43,9 +49,6 @@ namespace Project_Unorder.ObjectManage
         {
             SetBoxSize(new Vector2(20f, 20f), 3f, false);
         }
-
-        #endregion
-
 
         public void SetBoxSize(Vector2 newSize, float duration, bool isForce = false)
         {
@@ -60,15 +63,21 @@ namespace Project_Unorder.ObjectManage
 
         public void SetCollisionMode(bool value)
         {
-            if (_walls == null)
-            {
-                Debug.LogError("Colliders not Initialized");
-                return;
-            }
+            if (_walls == null) return;
 
+            _edgeBoxRenderer.color = value ? Color.white : Color.red;
             for (int i = 0; i < _walls.Length; i++)
             {
                 _walls[i].SwitchCollisionMode(value);
+            }
+
+            int propertyValue = value ? 1 : 0;
+            for (int i = 0; i < _barRenderers.Length; i++)
+            {
+                if (_barRenderers[i] != null)
+                {
+                    _barRenderers[i].sharedMaterial.SetInteger(_useLimitProperty, propertyValue);
+                }
             }
         }
 
@@ -80,17 +89,16 @@ namespace Project_Unorder.ObjectManage
 
                 float ratio = _currentDuration / _sizingDuration;
                 Vector2 newSize = Vector2.Lerp(_previousSize, _boxSize, ratio);
+
                 SetBoxSize(newSize, _boxThickness);
-                if (_currentDuration > _sizingDuration)
+
+                if (_currentDuration >= _sizingDuration)
                 {
                     _isSizing = false;
                     OnBoxSizingCompleteEvent?.Invoke();
                 }
             }
         }
-
-
-
 
         private void SetBoxSize(Vector2 newSize, float thickness)
         {
@@ -115,7 +123,5 @@ namespace Project_Unorder.ObjectManage
             _leftBar.localPosition = new Vector3(-halfX, 0f, 0f);
             _rightBar.localPosition = new Vector3(halfX, 0f, 0f);
         }
-
     }
-
 }
