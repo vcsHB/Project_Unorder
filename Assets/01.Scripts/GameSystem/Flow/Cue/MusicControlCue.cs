368eb5ea6fec7e47b1b6fd28326da699a59c8adb
vcs <hoonbot06@gmail.com>
2026-02-19 02:25:13 +0900

    fix: SoundController Duplicate, Cue Complete

--- Assets/01.Scripts/GameSystem/Flow/Cue/MusicControlCue.cs
+++ Assets/01.Scripts/GameSystem/Flow/Cue/MusicControlCue.cs
@@ -6,10 +6,11 @@ namespace Project_Unorder.FlowSystem
     public class MusicControlCue : FlowCue
     {
         [SerializeField] private SoundSO _newMusic;
-        
+
         public override void Execute()
         {
             SoundController.Instance.MusicPlayer.ChangeMusic(_newMusic, true);
+            InvokeCueComplete();
         }
     }
 }
